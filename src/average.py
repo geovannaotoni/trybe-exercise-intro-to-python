62e2635379ccd5e757b81216dae9dfcdaccffb31
Geovanna Otoni <geovannaotoni@gmail.com>
2023-10-18 08:33:34 -0300

    feat02: average

--- src/average.py
+++ src/average.py
@@ -2,4 +2,7 @@ from typing import List
 
 
 def find_average(numbers: List[int]) -> float:
-    raise NotImplementedError
+    sum_of_numbers = 0
+    for num in numbers:
+        sum_of_numbers += num
+    return sum_of_numbers / len(numbers)
